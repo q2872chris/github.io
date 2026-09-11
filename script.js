ecca11ab9110c012f0ac8ddb2e063796cf08a179
Chris <christopherwiseman17@gmail.com>
2026-09-11 21:06:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ async function loadMessages() {
 
     div.innerHTML = `
       <div class="username"></div>
-      <div class="text" style="width: 90%;"></div>
+      <div class="text" style="width: 600px !important;"></div>
       <div class="date"></div>
       <button class="copy-button">Copy</button>
     `;
